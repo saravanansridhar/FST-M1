abded7b1666beae6389b7a50bac3227ef588393d
saravanansridhar <58475726+saravanansridhar@users.noreply.github.com>
2021-10-25 13:20:38 +0530

    Add files via upload

---
+++ Python/Activities/act1.py
@@ -0,0 +1,6 @@
+name = input("Enter name")
+age = input("Enter age")
+
+yr = str((2021 - age) + 100 )
+
+print (name + " will be 100 years in "  +  yr)
---
+++ Python/Activities/act2.py
@@ -0,0 +1,16 @@
+x = int(input("Enter number:"))
+
+y = x % 2
+
+if  y > 0:
+    print("entered odd number")
+else:
+    print("entered even  number")
+
+
+
+
+
+
+
+
